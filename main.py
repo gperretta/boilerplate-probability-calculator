d2dc0c00a5829971159c74077bc688aca0c1f658
Gilda Perretta <perretta.gilda@gmail.com>
2023-04-25 19:53:38 +0200

    Add solution.

--- main.py
+++ main.py
@@ -2,15 +2,11 @@
 import prob_calculator
 from unittest import main
 
-prob_calculator.random.seed(95)
-hat = prob_calculator.Hat(blue=4, red=2, green=6)
-probability = prob_calculator.experiment(
-    hat=hat,
-    expected_balls={"blue": 2,
-                    "red": 1},
-    num_balls_drawn=4,
-    num_experiments=3000)
+hat = prob_calculator.Hat(black=6, red=4, green=3)
+probability = prob_calculator.experiment(hat=hat,
+                  expected_balls={"red":2,"green":1},
+                  num_balls_drawn=5,
+                  num_experiments=2000)
 print("Probability:", probability)
 
-# Run unit tests automatically
 main(module='test_module', exit=False)
--- prob_calculator.py
+++ prob_calculator.py
@@ -1,8 +1,49 @@
 import copy
 import random
-# Consider using the modules imported above.
+
 
 class Hat:
+  # :param items_contained: a variable number of arguments (** operator)
+  def __init__(self, **items_contained):
+    # instance variable - a list of strings
+    self.contents = []
+    for color, num in items_contained.items():
+      for i in range(num):
+        # each item in the 'contents' list should be a color name
+        self.contents.append(color)
+
+  # :param int amount: number of balls to draw
+  # :return string[] draw_list: return the balls removed from 'contents'
+  def draw(self, amount):
+    draw_list = []
+    # if the amount exceeds the available quantity, return all the balls
+    if amount >= len(self.contents):
+      return self.contents
+    for i in range(amount):
+      # the items should be removed randomly
+      item_index = random.randrange(len(self.contents))
+      removed = self.contents.pop(item_index)
+      draw_list.append(removed)
+    return draw_list
 
 
+# :param Hat hat: object containing balls that should be copied
+# :param dictionary expected_balls: group of balls to attempt to draw from the hat for the experiment
+# :param int num_balls_drawn: number of balls to draw out in each experiment
+# :param int num_experiments: number of experiments to perform
+# :return probability (result counter/num of experiments)
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  counter = 0
+  for _ in range(num_experiments):
+    # deep copy operation
+    # :note: assignment statements in Python do NOT copy objects, they create bindings (target-object)
+    copied = copy.deepcopy(hat)
+    temp = copied.draw(num_balls_drawn)
+    success = True
+    for key, value in expected_balls.items():
+      if temp.count(key) < value:
+        success = False
+        break
+    if success:
+      counter += 1
+  return counter / num_experiments
